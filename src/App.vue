d5d6e8352d3f76c4b556a17ae725aa5ef4460e50
Curtis Staples <cstaples@stanford.edu>
2018-05-23 23:31:40 -0700

    update

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <Head/>
+    <Main/>
+    <Foot/>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Head from '@/components/Head.vue'
+import Main from '@/components/Main.vue'
+import Foot from '@/components/Foot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Head,
+    Main,
+    Foot
+  }
+}
+</script>
+<style>
+
+</style>
