ac00bbb1c61487f1408a847ea055a087f8160357
Curtis Staples <cstaples@stanford.edu>
2018-07-12 16:14:03 -0700

    added users collectin to the userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,7 @@ import {store} from '../store/store.js'
 import firebase from '../store/firebase.js'
 const db = firebase.database;
 const students = db.collection('students');
+const users = db.collection('users');
 
 export default {
   currentUser: store.state.curUser.currentUser
