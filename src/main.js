015c75414c0c55c73b8b5c0eba661f02f54951d4
panda <SmelayaPanda@mail.ru>
2018-07-16 17:36:34 +0700

    automatic login

--- src/main.js
+++ src/main.js
@@ -18,5 +18,20 @@ new Vue({
   store,
   firebase,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    // This method is triggered automatically after the initial boot or manual entry/exit to the system
+    firebase.auth.onAuthStateChanged(user => {
+      console.log(user)
+      // Do anything what You want with this user.
+      // You can fetch user additional data from firestore, setup user data with vuex
+      // and push any route state
+      // These actions will mean automatic signin  after a refresh or a new visit
+      //
+      // this.$router.push('/anyRoute')
+      // this.$store.commit('setUser', user)
+      // ...
+      // console.log('User is loged in')
+    })
+  }
 })
--- src/store/firebase.js
+++ src/store/firebase.js
@@ -2,17 +2,20 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/storage'
 // Initialize Firebase
-  let config = {
-    apiKey: "AIzaSyCLK9emBDaQXX3TzTViZ-fIDNOs_orPw-k",
-    authDomain: "student-garage.firebaseapp.com",
-    databaseURL: "https://student-garage.firebaseio.com",
-    projectId: "student-garage",
-    storageBucket: "student-garage.appspot.com",
-    messagingSenderId: "912596358742"
-  };
-firebase.initializeApp(config);
+let config = {
+  apiKey: 'AIzaSyCLK9emBDaQXX3TzTViZ-fIDNOs_orPw-k',
+  authDomain: 'student-garage.firebaseapp.com',
+  databaseURL: 'https://student-garage.firebaseio.com',
+  projectId: 'student-garage',
+  storageBucket: 'student-garage.appspot.com',
+  messagingSenderId: '912596358742'
+}
+firebase.initializeApp(config)
+// Use this line if all of your Date types are in timestamps formats
+// firebase.firestore().settings({timestampsInSnapshots: true})
 
-export default{
+export default {
   database: firebase.firestore(),
-  storage : firebase.storage()
+  storage: firebase.storage(),
+  auth: firebase.auth()
 }
